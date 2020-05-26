bf067bbfaa52f8c1846ba5233f00f5c751ff429c
richard1230 <yongjinhu1230@gmail.com>
2020-05-26 17:16:55 +0800

    three icons are done!!

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,18 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/money.svg')
+require('icons/tag.svg')
+require('icons/chart.svg')
 
+
+//这里的console不可以删掉
+//如果不写console会发生TreeShaking现象
+//即:会将所对应的依赖删掉!
+//将会发生2件事:
+//1.symbol没了
+//2.svg会不显示
+// console.log(x,y)
 const NavWrapper = styled.nav`
 //border: 1px solid blue;
 line-height: 24px;
@@ -12,7 +23,16 @@ display: flex;
 > li{
 width: 33.333333%;
 text-align: center;
-padding: 16px;
+//padding: 16px;
+padding: 4px 0;
+display:flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+.icon{
+width: 24px;
+height: 24px
+ }
 }
 }
 `;
@@ -22,12 +42,21 @@ const Nav = ()=>{
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tag"/>
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart"/>
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
