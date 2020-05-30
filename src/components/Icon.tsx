75bb24e84aa7b5b5dd8a165c7bcc658fb98bb5dd
richard1230 <yongjinhu1230@gmail.com>
2020-05-30 15:41:49 +0800

    create Icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+//require 一个目录:google:how to require a full context
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
+
+
+type Props = {
+  name: string
+}
+
+//这里的类型开头必须是大小,也不能用any
+const Icon = (props: Props) => {
+  return (
+    <svg className="icon">
+      <use xlinkHref={'#' + props.name}/>
+    </svg>
+  );
+};
+
+export default Icon;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-require('icons/money.svg')
-require('icons/tag.svg')
-require('icons/chart.svg')
+import Icon from './Icon';
 
 
 //这里的console不可以删掉
@@ -37,31 +35,25 @@ height: 24px
 }
 `;
 
-const Nav = ()=>{
-  return(
+const Nav = () => {
+  return (
     <NavWrapper>
       <ul>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#tag"/>
-          </svg>
+          <Icon name="tag"/>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#money"/>
-          </svg>
+          <Icon name="money"/>
           <Link to="/money">记账页</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#chart"/>
-          </svg>
+          <Icon name="chart"/>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
     </NavWrapper>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
