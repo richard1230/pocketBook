1e11883987698da5d0de9e0bc2599ad8ab41ed93
richard1230 <yongjinhu1230@gmail.com>
2020-05-31 17:20:43 +0800

    create Layout.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -5,67 +5,62 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
-
-const Wrapper = styled.div`
-//border: 1px solid red; 
-height: 100vh; 
-display: flex;
-//使得结构变为上下结构
-flex-direction: column;
-//行高
- 
-`;
-
-const Main = styled.div`
-//border: 1px solid green;
-flex-grow: 1;
-overflow: auto;
-`;
+import Layout from 'components/Layout';
 
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
+      {/*<Wrapper>*/}
+      {/*  <Main>*/}
 
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            {/*注意:这里的字符串里面不要留空格,否则会出问题*/}
-            <Redirect exact from="/" to="/money"/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
+      <Switch>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        {/*注意:这里的字符串里面不要留空格,否则会出问题*/}
+        <Redirect exact from="/" to="/money"/>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
 
-        </Main>
-        <Nav/>
-      </Wrapper>
+      {/*</Main>*/}
+      {/*<Nav/>*/}
+      {/*</Wrapper>*/}
     </Router>
   );
 }
 
 function Statistics() {
-  return <h2>统计页面</h2>;
+  return (
+    <Layout>
+      <h2>统计页面</h2>
+    </Layout>
+  );
 }
 
 function Tags() {
-  return <h2>标签页面</h2>;
+  return (
+    <Layout>
+      <h2>标签页面</h2>
+    </Layout>
+  );
 }
 
 function Money() {
-  return <h2>记账页面</h2>;
+  return (
+    <Layout>
+      <h2>记账页面</h2>
+    </Layout>
+  );
 }
 
 function NoMatch() {
---
+++ src/components/Layout.tsx
@@ -0,0 +1,33 @@
+import Nav from './Nav';
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+//border: 1px solid red; 
+height: 100vh; 
+display: flex;
+//使得结构变为上下结构
+flex-direction: column;
+//行高
+ 
+`;
+
+const Main = styled.div`
+//border: 1px solid green;
+flex-grow: 1;
+overflow: auto;
+`;
+
+const Layout =(props:any )=>{
+  return (
+    <Wrapper>
+      <Main>
+        {props.children}
+      </Main>
+      <Nav/>
+    </Wrapper>
+  )
+}
+
+
+export default Layout
