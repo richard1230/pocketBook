96e8f16698c2d1e1d99cedb37d9e4a2d1534d621
richard1230 <yongjinhu1230@gmail.com>
2020-06-02 16:47:47 +0800

    Money.tsx 之 TagsSection

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,36 @@ import Money from './views/Money';
 import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
 
+
+const AppWrapper = styled.div`
+color: #333;
+`
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/tags">
-          <Tags/>
-        </Route>
-        <Route path="/money">
-          <Money/>
-        </Route>
-        <Route path="/statistics">
-          <Statistics/>
-        </Route>
-        {/*注意:这里的字符串里面不要留空格,否则会出问题*/}
-        <Redirect exact from="/" to="/money"/>
-        <Route path="*">
-          <NoMatch/>
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          {/*注意:这里的字符串里面不要留空格,否则会出问题*/}
+          <Redirect exact from="/" to="/money"/>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
+
   );
 }
 
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,16 +3,38 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TagsSection = styled.section`
+    
+    background:#FFFFFF;
+    //border: 1px solid red;
+    padding: 12px 16px;
+> ol{
+  margin: 0 -12px;
+  >li{
+  background:#D9D9D9; 
+  border-radius:18px ;
+  display: inline-block;
+  padding: 3px 18px;
+  font-size: 14px;
+  margin: 8px 12px;
+  }
+}
 
+> button{
+  background:none;
+  border:none;
+  padding: 2px 4px;
+  //加下划线
+  border-bottom: 1px solid #333;
+  color: #666;
+  margin-top: 8px ;
+}
 `;
-
 const NotesSection = styled.section`
 
 `;
 const CategorySection = styled.section`
 
 `;
-
 const NumberPadSection = styled.section`
 
 `;
