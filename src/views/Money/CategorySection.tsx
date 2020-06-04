438d8af5889cd984b96cef85d6fa7fdbbc7ba73d
richard1230 <yongjinhu1230@gmail.com>
2020-06-04 17:50:53 +0800

    type X = typeof categoryMap  type Y = keyof X

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -26,18 +26,22 @@ font-size: 24px;
 `;
 
 const CategorySection:React.FC = ()=>{
-  // const
+  const categoryMap = {'-':'支出','+':'收入'}
+  type X = typeof categoryMap
+  type Y = keyof X
+  //string[]:字符串数组;()[]:任意类型的数组;('-'|'+')[]:减和加的数组
+  const [categoryList] = useState<('-'|'+')[]>(['-','+'])
   const [category,setCategory] = useState('-') // + 表示收入  -表示支出
 
   return(
     <Wrapper>
       <ul>
-        <li  className={category=== '-' ? 'selected':''}
-              onClick={()=>{setCategory('-');}}
-        >支出</li>
-        <li  className={category=== '+' ? 'selected':''}
-             onClick={()=>{setCategory('+');}}
-        >收入</li>
+        {categoryList.map(c =>
+          <li  className={category=== c ? 'selected':''}
+               onClick={()=>{setCategory(c);}}
+          >{categoryMap[c]}</li>
+        )}
+
       </ul>
     </Wrapper>
   )
