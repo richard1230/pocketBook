cd6a3720d0b3c32a6039d9e29984715d5377e75e
richard1230 <yongjinhu1230@gmail.com>
2020-06-04 18:40:05 +0800

    numberPad 功能 work in process(计算等逻辑还未完成)

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
 display: flex;
 flex-direction: column;
 > .output{
@@ -66,5 +67,65 @@ box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
 }
 `;
 
+const NumberPadSection: React.FC = () => {
+  const [output, setOutput] = useState('0');
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent;
+    if (text === null) {
+      return;
+    }
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '.':
+        if (output === '0') {
+          setOutput(text);
+        } else {
+          setOutput(output + text);
+        }
+        break;
+      case '删除':
+        console.log('删除');
+        break;
+      case '清空':
+        console.log('清空');
+        break;
+      case 'ok':
+        console.log('ok');
+        break;
+    }
+  };
+  return (
+    <Wrapper>
+      <div className="output">
+        {output}
+      </div>
+      <div className="pad clearfix" onClick={onClickButtonWrapper}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="ok">OK</button>
+        <button className="zero">0</button>
+        <button className="dot">.</button>
+      </div>
+    </Wrapper>
+  );
+};
 
 export {NumberPadSection};
