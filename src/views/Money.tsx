1dbac1a11448d96006eea070d187716ad9f157aa
richard1230 <yongjinhu1230@gmail.com>
2020-06-05 18:17:44 +0800

    重构收集表单数据

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -19,6 +19,16 @@ function Money() {
     category: '-' as Category,
     amount: 0
   });
+
+  type Selected = typeof selected;
+  const onChangeState = (obj: Partial<Selected>) => {
+    setSelected({
+      ...selected,
+      ...obj
+    });
+  };
+
+
   return (
     <MyLayout>
       {selected.tags.join(',')}
@@ -29,30 +39,14 @@ function Money() {
       <hr/>
       {selected.amount}
       <TagsSection value={selected.tags}
-                   onChange={(tags) => setSelected({
-                     ...selected,
-                     tags: tags
-                   })
-                   }/>
+                   onChange={(tags) => onChangeState({tags})}/>
       <NoteSection value={selected.note}
-                   onChange={(note) => setSelected({
-                     ...selected,
-                     note: note
-                   })
-                   }/>
+                   onChange={(note) => onChangeState({note})}/>
       <CategorySection value={selected.category}
-                       onChange={(category) => setSelected({
-                         ...selected,
-                         category: category
-                       })}
-
-      />
+                       onChange={(category) => onChangeState({category})}/>
       <NumberPadSection value={selected.amount}
-                        onChange={(amount) => setSelected({
-                          ...selected,
-                          amount: amount
-                        })}
-                        onOk={()=>{}}
+                        onChange={(amount) => onChangeState({amount})}
+                        onOk={() => {}}
 
       />
 
