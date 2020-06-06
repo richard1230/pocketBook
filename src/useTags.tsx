8638b3bea70db3084cbcbc142c6f1384e5ba72ec
richard1230 <yongjinhu1230@gmail.com>
2020-06-06 15:27:07 +0800

    抽离useTags函数

---
+++ src/useTags.tsx
@@ -0,0 +1,9 @@
+import {useState} from 'react';
+
+
+const useTags = () => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  return {tags: tags, setTags: setTags};
+};
+
+export {useTags};
