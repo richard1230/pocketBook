0c9ee2f5cfa2d1b9f17d6977ad8e06f6670c5953
richard1230 <yongjinhu1230@gmail.com>
2020-06-08 17:58:16 +0800

    最后用 createId, 让id自增

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,14 +1,13 @@
 import {useState} from 'react';
 import {createId} from './lib/createId';
-import {Id} from './lib/Id';
 
 
 const useTags = () => {
   const [tags, setTags] = useState<{id:number;name:string}[]>([
-    {id:(new Id).value,name:'衣'},
-    {id:(new Id).value,name:'食'},
-    {id:(new Id).value,name:'住'},
-    {id:(new Id).value,name:'行'},
+    {id:createId(),name:'衣'},
+    {id:createId(),name:'食'},
+    {id:createId(),name:'住'},
+    {id:createId(),name:'行'},
   ]);
   return {tags: tags, setTags: setTags};
 };
--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import React from 'react';
 import {useTags} from '../../useTags';
 import {createId} from '../../lib/createId';
-import {Id} from '../../lib/Id';
 
 const Wrapper = styled.section`
     background:#FFFFFF;
@@ -52,7 +51,7 @@ const TagsSection: React.FC<Props> = (props) => {
     const tagName = window.prompt('新标签的名称为:');
     if (tagName !== null) {
       //...tags：ES6写法,表示之前的tags,
-      setTags([...tags,{id:(new Id).value,name:tagName}]);
+      setTags([...tags,{id:createId(),name:tagName}]);
     }
   };
   const onToggleTag = (tagId: number) => {
